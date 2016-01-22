06c31e6d41de5b24aa4f9f04fc62ea91ba5b11dc
CaseyL <tolerocasey@gmail.com>
2016-01-22 08:07:37 -0700

    Initial Commit

--- retroCalculator/ViewController.swift
+++ retroCalculator/ViewController.swift
@@ -7,19 +7,126 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    enum Operation: String {
+        case Multiply = "*"
+        case Divide = "/"
+        case Add = "+"
+        case Subtract = "-"
+        case Empty = ""
+    }
 
+    @IBOutlet weak var outputLbl: UILabel!
+    
+    var runningNumber = ""
+    var leftNumStr = ""
+    var rightNumStr = ""
+    var result = 0.0
+    var resultStr = ""
+    var currentOperation: Operation!
+    
+    var btnSound: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let path = NSBundle.mainBundle().pathForResource("btn", ofType: "wav")
+        
+        let soundUrl = NSURL(fileURLWithPath: path!)
+        
+        do{
+            try btnSound = AVAudioPlayer(contentsOfURL: soundUrl)
+            btnSound.prepareToPlay()
+        }catch let err as NSError {
+            print(err.debugDescription)
+        }
+        
+        currentOperation = Operation.Empty
+        outputLbl.text = "0"
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func buttonPressed(btn: UIButton!){
+        playSound()
+        runningNumber += "\(btn.tag)"
+        outputLbl.text = runningNumber
+        
     }
 
+    @IBAction func onMultiplyPressed(sender: AnyObject) {
+        performOperation(Operation.Multiply)
+    }
 
+    @IBAction func onDividePressed(sender: AnyObject) {
+        performOperation(Operation.Divide)
+    }
+
+    @IBAction func onAddPressed(sender: AnyObject) {
+        performOperation(Operation.Add)
+    }
+       
+    @IBAction func onSubtractPressed(sender: AnyObject) {
+        performOperation(Operation.Subtract)
+    }
+    
+    @IBAction func onEqualsPressed(sender: AnyObject) {
+        performOperation(currentOperation)
+    }
+    
+    @IBAction func onClearPressed(sender: AnyObject){
+        clearStack()
+    }
+    
+    func performOperation(op:Operation){
+        playSound()
+        if runningNumber != ""{
+            if currentOperation != Operation.Empty && leftNumStr != ""{
+                rightNumStr = runningNumber
+                if currentOperation == Operation.Multiply{
+                    resultStr = "\(Double(leftNumStr)! * Double(rightNumStr)!)"
+                    result = Double(leftNumStr)! * Double(rightNumStr)!
+                }else if currentOperation == Operation.Divide{
+                    resultStr = "\(Double(leftNumStr)! / Double(rightNumStr)!)"
+                    result = Double(leftNumStr)! / Double(rightNumStr)!
+                }else if currentOperation == Operation.Add{
+                    resultStr = "\(Double(leftNumStr)! + Double(rightNumStr)!)"
+                    result = Double(leftNumStr)! + Double(rightNumStr)!
+                }else if currentOperation == Operation.Subtract{
+                    resultStr = "\(Double(leftNumStr)! - Double(rightNumStr)!)"
+                    result = Double(leftNumStr)! - Double(rightNumStr)!
+                }
+                updateStack()
+                outputLbl.text = String.localizedStringWithFormat("%.3f", result)
+                
+            }else{
+                resultStr = runningNumber
+                updateStack()
+            }
+
+        }
+        
+        currentOperation = op
+    }
+    func clearStack(){
+        leftNumStr = ""
+        rightNumStr = ""
+        runningNumber = ""
+        resultStr = ""
+        currentOperation = Operation.Empty
+        outputLbl.text = "0"
+    }
+    func playSound(){
+        if btnSound.playing{
+            btnSound.stop()
+        }
+        btnSound.play()
+    }
+    func updateStack(){
+        leftNumStr = resultStr
+        runningNumber = ""
+    }
+    
 }
 
